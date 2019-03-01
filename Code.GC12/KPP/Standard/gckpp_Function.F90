b9b5d478d8434a4995ae7f13f6e4d196c3877e4a
Lu Shen <lshen729@gmail.com>
2019-02-28 23:13:04 -0500

    lshen

--- Code.GC12/KPP/Standard/gckpp_Function.F90
+++ Code.GC12/KPP/Standard/gckpp_Function.F90
@@ -21166,7 +21166,7 @@ FUNCTION Determine_type (Prate,Lrate,LS_change) RESULT( flag )
   INTEGER ::Vdot(NVAR),PP(10),ap,flag,I,LS_change
   flag=0
   Vdot=0
-  WHERE (abs(Prate)>=100 .or. abs(Lrate)>=100) Vdot=1
+  WHERE (abs(Prate)>=thresholds .or. abs(Lrate)>=thresholds) Vdot=1
   PP(1)=SUM(Vdot(LS_ind_1))
   PP(2)=SUM(Vdot(LS_ind_2))
   PP(3)=SUM(Vdot(LS_ind_3))
